b7eef1adf69a9c3c8654bf50eb077391fc542595
Ran Crook <rcrook@opploans.com>
2023-08-17 13:25:38 -0500

    To-do list

---
+++ pages/todo.js
@@ -0,0 +1,63 @@
+import Head from 'next/head';
+import { useState } from 'react';
+
+function Item({ key, description, handleDelete }) {
+  return (
+    <li key={key} >
+      {description}
+      <button onClick={handleDelete}>
+        ×
+      </button>
+    </li>
+  )
+}
+
+export default function Todo() {
+  const [key, setKey] = useState(0);
+  const [item, setItem] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const handleCreate = () => {
+    if (item === '') return;
+
+    setTodoList(todoList.concat({ id: key, description: item }));
+    setKey(key + 1);
+    setItem('');
+  }
+
+  const handleDelete = (id) => {
+    setTodoList(
+      todoList.filter((item) => id !== item.id)
+    );
+  }
+
+  return (
+    <>
+      <Head>
+        <title>To-Do List</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          To-Do List
+        </h1>
+
+        <input type="text" value={item} onChange={(e) => setItem(e.target.value)} />
+        <button onClick={handleCreate}>Submit</button>
+
+        <ol>
+          {
+            todoList.map((item) => (
+              <Item
+                key={item.id}
+                description={item.description}
+                handleDelete={() => handleDelete(item.id)}
+              />
+            ))
+          }
+        </ol>
+      </main>
+    </>
+  )
+}
